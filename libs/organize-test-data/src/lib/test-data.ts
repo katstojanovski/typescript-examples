89e589d32f2e48276cd296af111d9a0f708ae304
Katarina Stojanovski <7660910+katstojanovski@users.noreply.github.com>
2026-03-09 22:49:14 +0100

    fixed partial deep issue

--- libs/organize-test-data/src/lib/test-data.ts
+++ libs/organize-test-data/src/lib/test-data.ts
@@ -1,5 +1,6 @@
 import { CreateOrderProps, Order } from './Order';
 import merge from 'lodash.merge';
+import { PartialDeep } from 'type-fest';
 
 export const ORDER_ID = '1234567890';
 export const LINE_ITEM_ID = 1;
@@ -9,17 +10,8 @@ export const ORDERED_QUANTITY = 5;
 export const UNIT_PRICE = 10.35;
 export const CUSTOMER_ID = '95446fe0-5177-427b-bac4-bd8b7cc7f4ab';
 
-// Custom type that ensures arrays have partial items
-type DeepPartialWithArrays<T> = T extends any[]
-  ? Array<DeepPartialWithArrays<T[number]>>
-  : T extends object
-    ? {
-        [P in keyof T]?: DeepPartialWithArrays<T[P]>;
-      }
-    : T;
-
 export const orderFixture = (
-  overrides?: DeepPartialWithArrays<CreateOrderProps>,
+  overrides?: PartialDeep<CreateOrderProps, { recurseIntoArrays: true }>,
 ): Order => {
   const defaults: CreateOrderProps = {
     id: '1234567890',
